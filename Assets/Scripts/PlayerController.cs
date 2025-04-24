48573bb6ec7976a7ab04612fc9de1e20a37b6fa0
ImanolRelloso <imanol_relloso@euneiz.com>
2025-04-24 22:48:18 +0200

    UpgradeMenu added with functional upgrades (except speed)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private double Cadence;
     [SerializeField] private float moveSpeed;
 
-    [SerializeField] private int XP;
+    [SerializeField] public int XP;
 
     void Update()
     {
@@ -75,12 +75,12 @@ public class PlayerController : MonoBehaviour
     { 
     }
 
-    int HealthLvl;
-    int DamageLvl;
-    int GreedLvl;
-    int CadenceLvl;
-    int moveSpeedLvl;
-    void LevelUp(int opcion)
+    public int HealthLvl = 1;
+    public int DamageLvl = 1;
+    public int GreedLvl = 1;
+    public int CadenceLvl = 1;
+    public int moveSpeedLvl = 1;
+    public void LevelUp(int opcion)
     {
         switch (opcion)
         {
@@ -89,21 +89,25 @@ public class PlayerController : MonoBehaviour
                 Health++;
                 MaxHealth++;
                 HealthLvl++;
+                Debug.Log(MaxHealth);
                 break;
             //Damage
             case 2:
                 Damage = Damage + Damage * 0.2;
                 DamageLvl++;
+                Debug.Log(Damage);
                 break;
             //Greed
             case 3:
                 Greed = Greed + 0.3;
                 GreedLvl++;
+                Debug.Log(Greed);
                 break;
             //Cadence
             case 4:
                 Cadence = Cadence + Cadence * 0.15;
                 CadenceLvl++;
+                Debug.Log(Cadence);
                 break;
             //Speed
             case 5:
--- Assets/Scripts/UpgradeMenu.cs
+++ Assets/Scripts/UpgradeMenu.cs
@@ -1,10 +1,11 @@
 using JetBrains.Annotations;
+using System.Xml.XPath;
 using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
     public GameObject upgradeMenu;
-
+    public PlayerController playerController;
     public void Pause()
     { 
         upgradeMenu.SetActive(true);
@@ -17,6 +18,10 @@ public class UpgradeMenu : MonoBehaviour
         Time.timeScale = 1;
     }
 
+    private void Start()
+    {
+        upgradeMenu.SetActive(false);
+    }
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -30,5 +35,46 @@ public class UpgradeMenu : MonoBehaviour
                 Pause();
             }
         }
+    }
+
+    public void HealthUpgrade()
+    {
+        if (playerController.XP >= 100 * playerController.HealthLvl)
+        {
+            playerController.XP = playerController.XP - 100 * playerController.HealthLvl;
+            playerController.LevelUp(1);
+        }
+    }
+
+    public void DamageUpgrade()
+    {
+        if (playerController.XP >= 100 * playerController.DamageLvl)
+        {
+            playerController.XP = playerController.XP - 100 * playerController.DamageLvl;
+            playerController.LevelUp(2);
+        }
+    }
+
+    public void GreedUpgrade()
+    {
+        if (playerController.XP >= 100 * playerController.GreedLvl)
+        {
+            playerController.XP = playerController.XP - 100 * playerController.GreedLvl;
+            playerController.LevelUp(3);
+        }
+    }
+
+    public void CadenceUpgrade()
+    {
+        if (playerController.XP >= 100 * playerController.CadenceLvl)
+        {
+            playerController.XP = playerController.XP - 100 * playerController.CadenceLvl;
+            playerController.LevelUp(4);
+        }
+    }
+
+    public void SpeedUpgrade()
+    {
+        //Aquí va la mejora de velocidad para cuando se ponga la nueva velocidad
     }
 }
