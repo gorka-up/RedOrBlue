e1df0080539681d40186a2ea78e92cf234e0bcca
ImanolRelloso <imanol_relloso@euneiz.com>
2025-04-30 11:10:50 +0200

    EnemyBullet do damage and destroy

--- Assets/Scripts/EnemyBulletController.cs
+++ Assets/Scripts/EnemyBulletController.cs
@@ -5,19 +5,33 @@ public class EnemyBulletController : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     public float bulletForce;
+    private PlayerController playerController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
 
         Vector3 direction = player.transform.position - transform.position;
         rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * bulletForce;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-
+        switch (collision.gameObject.tag)
+        {
+            case "Wall":
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+            case "Player":
+                {
+                    playerController.GetHurt();
+                    Destroy(gameObject);
+                    break;
+                }
+        }
     }
 }
