ef1a516e8b4b26a766c8ceeea3c581b41871209a
ImanolRelloso <imanol_relloso@euneiz.com>
2025-05-06 17:58:34 +0200

    Main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}
